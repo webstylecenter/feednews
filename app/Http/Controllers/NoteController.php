b082130cfe7e18ab434ba1dd4eb32d24b64e7fa4
Peter van Dam <peter@petervdam.nl>
2020-12-30 11:39:21 +0100

    Added note save and remove functionality

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,17 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class NoteController extends BaseController
 {
-    public function save()
+    public function save(Request $request, Note $note): array
     {
-        // TODO: Add functionality to method
+        $request->validate([
+            'note' => 'required'
+        ]);
+
+        if ($request->get('id')) {
+            $note = Note::where('id', '=', $request->get('id'))->where('user_id', '=', Auth::user()->id);
+
+            if (!$note) {
+                abort(403);
+            }
+        } else {
+            $note->user_id = Auth::user()->id;
+        }
+
+        $note->name = $request->get('name');
+        $note->content = $request->get('note');
+        $note->position = $request->get('position') ?? 0;
+        $note->save();
+
+        return [
+            'status' => 'success',
+            'data' => [
+                'id' => $note->id
+            ]
+        ];
     }
 
-    public function remove()
+    public function remove(Request $request): array
     {
-        // TODO: Add functionality to method
+        $request->validate([
+            'id' => ['required', 'integer']
+        ]);
+
+        $note = Note::where('id', '=', $request->get('id'))->where('user_id', '=', Auth::user()->id);
+
+        if (!$note) {
+            abort(403);
+        }
+
+        $note->delete();
+
+        return [
+            'status' => 'success'
+        ];
     }
 }
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
+use App\Repositories\ChecklistRepository;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class WelcomeController extends BaseController
 {
-    public function index(): View
+    public function index(ChecklistRepository $checklistRepository): View
     {
         app('debugbar')->disable();
 
         return view('welcome.index', [
             'bodyClass' => 'page--homepage',
-            'notes' => [],
-            'todos' => []
+            'notes' => Note::where('user_id', '=', Auth::user()->id)->get(),
+            'todos' => $checklistRepository->getTodos()
         ]);
     }
 }
