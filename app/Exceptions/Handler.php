2ee1debb30af498f147737875ef57d052ea06adb
Peter van Dam <peter@petervdam.nl>
2021-02-16 18:51:28 +0100

    Added Sentry for error logging

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -37,4 +37,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $e)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
+
+        parent::report($e);
+    }
 }
