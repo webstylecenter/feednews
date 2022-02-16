6bfcf61a2c811576a6cc5ea161fcce45e18179c1
Peter van Dam <peter@petervdam.nl>
2022-02-16 18:28:48 +0100

    Add shedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateFeeds;
+use App\Console\Commands\UpdateWeatherInfo;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +17,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(UpdateFeeds::class)->everyTenMinutes();
+        $schedule->command(UpdateWeatherInfo::class)->everyThirtyMinutes();
     }
 
     /**
